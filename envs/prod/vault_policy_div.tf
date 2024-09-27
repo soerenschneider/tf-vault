8dacf944e7ed3afb1d1c013af34617535c9c1a1a
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-09-27 15:50:48 +0200

    yeah

--- envs/prod/vault_policy_div.tf
+++ envs/prod/vault_policy_div.tf
@@ -1,34 +1,7 @@
 resource "vault_policy" "kv2_machine" {
   name   = "kv2_machine"
   policy = <<EOT
-path "${vault_mount.kv.path}/data/machine/{{identity.entity.aliases.${vault_auth_backend.approle.accessor}.metadata.host}}/*" {
-  capabilities = ["read"]
-}
-EOT
-}
-
-resource "vault_policy" "kv2_machine_vserver_ez" {
-  name   = "kv2_machine_vserver_ez"
-  policy = <<EOT
-path "${vault_mount.kv.path}/data/machine/vserver.ez.soeren.cloud/*" {
-  capabilities = ["read"]
-}
-EOT
-}
-
-resource "vault_policy" "kv2_machine_vserver_pt" {
-  name   = "kv2_machine_vserver_pt"
-  policy = <<EOT
-path "${vault_mount.kv.path}/data/machine/vserver.pt.soeren.cloud/*" {
-  capabilities = ["read"]
-}
-EOT
-}
-
-resource "vault_policy" "kv2_machine_vserver_dd" {
-  name   = "kv2_machine_vserver_dd"
-  policy = <<EOT
-path "${vault_mount.kv.path}/data/machine/vserver.dd.soeren.cloud/*" {
+path "${vault_mount.kv.path}/data/machine/{{identity.entity.metadata.host}}/*" {
   capabilities = ["read"]
 }
 EOT
